f8e909e4c464020c0739f3b70f97da9f57bfa7b5
Mahmoud Ahmed <mahmoudxbahnasawy@outlook.com>
2024-10-26 21:00:09 +0300

    feat: Add game initialization and configuration constants
    - Define constants for game configuration:
      - NUM_CARS, NUM_ROADS, WINDOW_WIDTH, WINDOW_HEIGHT, WINNING_POSITION
    - Declare global variables for game elements:
      - Arrays for roads and cars, and a Frog instance
      - Game over state
    - Add function prototypes for initialization:
      - initializeRoads(), initializeCars(), initializeFrog(), initializeGameState()

--- game_config.h
+++ game_config.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#include "structures.h"
+#include <stdbool.h>
+
+#define NUM_CARS 5
+#define NUM_ROADS 5
+#define WINDOW_WIDTH 1000
+#define WINDOW_HEIGHT 800
+#define WINNING_POSITION 335
+
+Road roads[NUM_ROADS];
+Car cars[NUM_CARS];
+Frog frog;
+bool gameOver;
+
+void initializeRoads();
+void initializeCars();
+void initializeFrog();
+void initializeGameState();
