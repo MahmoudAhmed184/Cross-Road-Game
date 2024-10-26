675ce88661f7399b393b1973017303675d8f5046
Mahmoud Ahmed <mahmoudxbahnasawy@outlook.com>
2024-10-26 21:02:24 +0300

    feat: Add rendering function declarations for game elements in render.h
    - Declare function prototypes for rendering:
      - drawCarBody(), drawCarWheels(), drawCarWindows(), drawCarLights(), drawCar()
    - Introduce function prototype for road and frog rendering:
      - drawRoad(), drawFrog()

--- render.h
+++ render.h
@@ -1,1 +1,11 @@
 #pragma once
+
+#include "game_config.h"
+
+void drawCarBody(const Car *car);
+void drawCarWheels(const Car *car);
+void drawCarWindows(const Car *car);
+void drawCarLights(const Car *car);
+void drawCar(const Car *car);
+void drawRoad(const Road *road);
+void drawFrog(const Frog *frog);
