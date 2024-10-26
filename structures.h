72746cda1e680c22c65f268e28771adf76136680
Mahmoud Ahmed <mahmoudxbahnasawy@outlook.com>
2024-10-26 20:59:28 +0300

    feat: Define core data structures for the 2D Cross Road Game
    - Add structures for game elements:
      - Point: Represents a coordinate in 2D space.
      - Road: Represents a road with dimensions.
      - Color: Represents RGB color values.
      - Car: Represents a car with position, size, color, road reference, and speed.
      - Frog: Represents the player character with position, size, color, and speed.

--- structures.h
+++ structures.h
@@ -1,1 +1,42 @@
 #pragma once
+
+#include <GL/glut.h>
+
+typedef struct
+{
+    GLfloat x;
+    GLfloat y;
+} Point;
+
+typedef struct
+{
+    Point start;
+    GLfloat width;
+    GLfloat height;
+} Road;
+
+typedef struct
+{
+    GLfloat red;
+    GLfloat green;
+    GLfloat blue;
+} Color;
+
+typedef struct
+{
+    Point start;
+    GLfloat width;
+    GLfloat height;
+    Color color;
+    Road *road;
+    GLint speed;
+} Car;
+
+typedef struct
+{
+    Point start;
+    GLfloat width;
+    GLfloat height;
+    Color color;
+    GLint speed;
+} Frog;
